4a7373e6d75fb443e3b71289bb8983a4b5041102
Dale Harvey <dale@arandomurl.com>
2014-01-18 02:47:32 +0000

    FIRST

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-done
-====
+# Done!
+
+A simple PouchDB based todo list based off Vanilla JavaScript TodoMVC (http://todomvc.cmo)
---
+++ js/store.pouchdb.js
@@ -0,0 +1,73 @@
+(function (window) {
+
+  'use strict';
+
+  function Store(name, cb) {
+    this.name = name;
+    window.db = this.db = new PouchDB(name, cb);
+  }
+
+  function pdocTodoc(doc) {
+    doc.id = doc._id;
+    delete doc._rev;
+    delete doc._id;
+    return doc;
+  }
+
+  Store.prototype.find = function(query, callback) {
+    this.db.allDocs({include_docs: true}, function(err, docs) {
+      var matches = [];
+      docs.rows.forEach(function(row) {
+        var doc = row.doc;
+        for (var q in query) {
+          if (query[q] !== doc[q]) {
+            return;
+          }
+        }
+        matches.push(pdocTodoc(doc));
+      });
+      callback(matches);
+    });
+  };
+
+  Store.prototype.findAll = function(callback) {
+    this.db.allDocs({include_docs: true}, function(err, docs) {
+      callback(docs.rows.map(function(row) {
+        return pdocTodoc(row.doc);
+      }).reverse());
+    });
+  };
+
+  Store.prototype.save = function (id, updateData, callback) {
+    if (typeof id !== 'object') {
+      this.db.get(id, (function(err, data) {
+        for (var x in updateData) {
+          data[x] = updateData[x];
+          this.db.put(data, callback);
+        }
+      }).bind(this));
+    } else {
+      callback = updateData;
+      updateData = id;
+      updateData._id = new Date().getTime().toString();
+      this.db.put(updateData, function(err) {
+        callback();
+      });
+    }
+  };
+
+  Store.prototype.remove = function (id, callback) {
+    this.db.get(id, (function(err, data) {
+      this.db.remove(data, callback);
+    }).bind(this));
+  };
+
+  Store.prototype.drop = function (callback) {
+    PouchDB.destroy(this.name, callback);
+  };
+
+  // Export to window
+  window.app = window.app || {};
+  window.app.Store = Store;
+
+})(window);
