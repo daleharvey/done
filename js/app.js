0439c43b9f9ec4d83a2ebb10e2447d84b9b615d6
Dale Harvey <dale@arandomurl.com>
2014-01-18 05:22:54 +0000

    Get first ugly version of sync in

--- js/app.js
+++ js/app.js
@@ -19,4 +19,81 @@
     todo.controller.setView(document.location.hash);
   }.bind(this));
 
+  // This is a very quick and dirty sync setup
+
+  // Host that the couch-persona server is running on
+  var authHost = 'http://persona.pouchdb.com';
+  var remoteCouch;
+  var replication;
+
+  function sync(details) {
+    if (replication) return;
+    var remote = new PouchDB(details.url, {headers: {'Cookie': details.auth}});
+    var opts = {
+      continuous: true,
+      complete: syncError
+    };
+    todo.storage.db.replicate.to(remote, opts);
+    todo.storage.db.replicate.from(remote, opts);
+  }
+
+  function syncError() {}
+  function cancelSync() {}
+
+  var loggedIn = function(result) {
+    sync({
+      url: result.dbUrl,
+      auth: result.authToken
+    });
+  };
+
+  var loggedOut = function() {
+    cancelSync();
+  };
+
+  function simpleXhrSentinel(xhr) {
+    return function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          var result = {};
+          try {
+            result = JSON.parse(xhr.responseText);
+          } catch(e) {}
+          loggedIn(result);
+        } else {
+          loggedOut();
+          navigator.id.logout();
+        }
+      }
+    };
+  }
+
+  function verifyAssertion(assertion) {
+    var xhr = new XMLHttpRequest();
+    var param = 'assert=' + assertion;
+    xhr.open('POST', authHost + '/persona/sign-in', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Content-length", param.length);
+    xhr.setRequestHeader("Connection", "close");
+    xhr.send(param);
+    xhr.onreadystatechange = simpleXhrSentinel(xhr);
+  }
+
+  function signoutUser() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", authHost + '/persona/sign-out', true);
+    xhr.send(null);
+    xhr.onreadystatechange = simpleXhrSentinel(xhr);
+  }
+
+  navigator.id.watch({
+    onlogin: verifyAssertion,
+    onlogout: signoutUser
+  });
+
+  var signinLink = document.getElementById('signin');
+  var signoutLink = document.getElementById('signout');
+  signinLink.onclick = function() { navigator.id.request(); };
+  signoutLink.onclick = function() { navigator.id.logout(); };
+
 })();
