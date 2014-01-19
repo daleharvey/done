922fa105c277eb1b81b409da48b9503584c790cf
Dale Harvey <dale@arandomurl.com>
2014-01-19 09:04:15 +0000

    Add a spinner

--- js/app.js
+++ js/app.js
@@ -28,6 +28,27 @@
   var signoutLink = document.getElementById('signout');
   var username = document.getElementById('username');
 
+  var opts = {
+    lines: 9, // The number of lines to draw
+    length: 5, // The length of each line
+    width: 3, // The line thickness
+    radius: 5, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 100, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  var target = document.getElementById('spinner');
+  var spinner = new Spinner(opts);
+
   // Update the UI whenever we see changes, this is gonna cause things
   // to refresh twice when you make a change, sorry
   todo.storage.db.changes({
@@ -39,8 +60,27 @@
     }
   });
 
+  var spinnerActive = false;
+  var spinnerTimeout = null;
+
+  function dataNotSyncing() {
+    spinnerActive = false;
+    spinnerTimeout = null;
+    spinner.stop();
+  }
+
+  function dataSyncing() {
+    if (!spinnerActive) {
+      spinnerActive = true;
+      spinner.spin(target);
+    } else {
+      clearTimeout(spinnerTimeout);
+    }
+    spinnerTimeout = setTimeout(dataNotSyncing, 2000);
+  };
+
   function sync(url) {
-    var opts = {continuous: true};
+    var opts = {continuous: true, onChange: dataSyncing};
     todo.storage.db.replicate.to(url, opts);
     todo.storage.db.replicate.from(url, opts);
   }
