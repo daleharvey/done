fae87d788f5197b20d3b7d4f76000b9a5ddf79ae
Nick Colley <nickcolley7@gmail.com>
2014-02-23 20:09:06 +0000

    (#1) - Switch to CSS spinners

--- js/app.js
+++ js/app.js
@@ -28,26 +28,7 @@
   var signoutLink = document.getElementById('signout');
   var username = document.getElementById('username');
 
-  var opts = {
-    lines: 9, // The number of lines to draw
-    length: 5, // The length of each line
-    width: 3, // The line thickness
-    radius: 5, // The radius of the inner circle
-    corners: 1, // Corner roundness (0..1)
-    rotate: 0, // The rotation offset
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#000', // #rgb or #rrggbb or array of colors
-    speed: 1, // Rounds per second
-    trail: 100, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: false, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000)
-    top: 'auto', // Top position relative to parent in px
-    left: 'auto' // Left position relative to parent in px
-  };
-  var target = document.getElementById('spinner');
-  var spinner = new Spinner(opts);
+  var spinner = document.querySelector('.spinner');
 
   // Update the UI whenever we see changes, this is gonna cause things
   // to refresh twice when you make a change, sorry
@@ -66,13 +47,13 @@
   function dataNotSyncing() {
     spinnerActive = false;
     spinnerTimeout = null;
-    spinner.stop();
+    spinner.style.visibility = 'hidden';
   }
 
   function dataSyncing() {
     if (!spinnerActive) {
       spinnerActive = true;
-      spinner.spin(target);
+      spinner.style.visibility = 'visible';
     } else {
       clearTimeout(spinnerTimeout);
     }
