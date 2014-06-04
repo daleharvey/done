096ef8cee504b3121289e6ca9ef75cf14cd12f47
Dale Harvey <dale@arandomurl.com>
2014-06-04 23:59:44 +0100

    sotr demo

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
     this.controller = new app.Controller(this.model, this.view);
   }
 
-  var todo = new Todo('todos-vanillajs');
+  var todo = new Todo('todos');
 
   window.addEventListener('load', function () {
     todo.controller.setView(document.location.hash);
@@ -21,19 +21,10 @@
 
   // This is a very quick and dirty sync setup
 
-  // where is my db servive at
-  var authHost = 'http://persona.pouchdb.com';
-  var syncDom = document.getElementById('sync');
-  var signinLink = document.getElementById('signin');
-  var signoutLink = document.getElementById('signout');
-  var username = document.getElementById('username');
-
-  var spinner = document.querySelector('.spinner');
-
   // Update the UI whenever we see changes, this is gonna cause things
   // to refresh twice when you make a change, sorry
   todo.storage.db.changes({
-    since: 'latest',
+    since: 'now',
     continuous: true,
     onChange: function(change) {
       todo.controller.setView(document.location.hash);
@@ -41,82 +32,15 @@
     }
   });
 
-  var spinnerActive = false;
-  var spinnerTimeout = null;
 
-  function dataNotSyncing() {
-    spinnerActive = false;
-    spinnerTimeout = null;
-    spinner.style.visibility = 'hidden';
+  var url = 'http://couchdb.pouchdb.com/sotr';
+
+  function sync() {
+    todo.storage.db.sync(url, {live: true}).on('error', function() {
+      setTimeout(sync, 1000);
+    });
   }
 
-  function dataSyncing() {
-    if (!spinnerActive) {
-      spinnerActive = true;
-      spinner.style.visibility = 'visible';
-    } else {
-      clearTimeout(spinnerTimeout);
-    }
-    spinnerTimeout = setTimeout(dataNotSyncing, 2000);
-  };
-
-  function sync(url) {
-    var opts = {continuous: true, onChange: dataSyncing};
-    todo.storage.db.replicate.to(url, opts);
-    todo.storage.db.replicate.from(url, opts);
-  }
-
-  var loggedIn = function(result) {
-    syncDom.dataset.loggedin = 'true';
-    username.textContent = result.name
-    sync(result.db);
-  };
-
-  var loggedOut = function() {
-    syncDom.dataset.loggedin = 'false';
-  };
-
-  function simpleXhrSentinel(xhr) {
-    return function() {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          var result = {};
-          try {
-            result = JSON.parse(xhr.responseText);
-          } catch(e) {}
-          loggedIn(result);
-        } else {
-          loggedOut();
-          navigator.id.logout();
-        }
-      }
-    };
-  }
-
-  function verifyAssertion(assertion) {
-    var xhr = new XMLHttpRequest();
-    var param = 'assert=' + assertion;
-    xhr.open('POST', authHost + '/persona/sign-in', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-length", param.length);
-    xhr.setRequestHeader("Connection", "close");
-    xhr.send(param);
-    xhr.onreadystatechange = simpleXhrSentinel(xhr);
-  }
-
-  function signoutUser() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", authHost + '/persona/sign-out', true);
-    xhr.send(null);
-    xhr.onreadystatechange = simpleXhrSentinel(xhr);
-  }
-
-  navigator.id.watch({
-    onlogin: verifyAssertion,
-    onlogout: signoutUser
-  });
-
-  signinLink.onclick = function() { navigator.id.request(); };
-  signoutLink.onclick = function() { navigator.id.logout(); };
+  //sync();
 
 })();
