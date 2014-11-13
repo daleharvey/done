8ca614c78c2ed641040a18c0594eca9040980b69
Dale Harvey <dale@arandomurl.com>
2014-11-13 15:38:48 +0100

    Added some stuff

--- js/done.js
+++ js/done.js
@@ -1,33 +1,15 @@
 'use strict';
 
-var syncStatus = document.getElementById('syncStatus');
-var syncDialog = document.getElementById('syncDialog');
-var lists = document.getElementById('lists');
-var items = document.getElementById('items');
-var todoItems = document.getElementById('todo-items');
-var doneItems = document.getElementById('done-items');
-var newListForm = document.getElementById('newList');
-var newItemForm = document.getElementById('newItem');
-var newItemText = document.getElementById('newItemText');
-var listName = document.getElementById('listName');
-var listHeader = document.getElementById('listHeader');
-
-var currentIndex = 0;
-
-function newTodo(obj) {
-  var def = {
-    category: 'default',
-    status: 'todo',
-    title: '',
-    index: ++currentIndex,
-    created: Date.now()
+// Yay jQuery
+var $ = (function() {
+  var elements = {};
+  return function(id) {
+    if (!(id in elements)) {
+      elements[id] = document.getElementById(id);
+    }
+    return elements[id];
   };
-  Object.keys(obj).forEach(function(key) {
-    def[key] = obj[key];
-  });
-  def._id = 'items_' + def.category + '_' + def.created;
-  return def;
-}
+})();
 
 function createListItem(doc) {
   var li = document.createElement('li');
@@ -35,22 +17,289 @@ function createListItem(doc) {
   var btn = document.createElement('button');
   li.dataset.updated = doc.updated;
   li.dataset.status = doc.status;
-  li.dataset.action = 'edit';
-  li.dataset.id = doc._id;
   li.dataset.index = doc.index;
   btn.innerHTML = '✓';
   btn.dataset.id = doc._id;
   btn.dataset.action = 'done';
+  a.href = '#/list/' + doc.category + '/edit/' + doc._id;
   a.innerHTML = doc.title;
   li.appendChild(a);
   li.appendChild(btn);
   return li;
 }
 
-function processChange(doc) {
 
-  if (doc.index > currentIndex) {
-    currentIndex = doc.index;
+function updateBackground(list) {
+  var rgb1 = [118,195,45];
+  var rgb2 = [195,237,131];
+  var numItems = list.childNodes.length;
+  var steps = [
+    (rgb2[0] - rgb1[0]) / numItems,
+    (rgb2[1] - rgb1[1]) / numItems,
+    (rgb2[2] - rgb1[2]) / numItems
+  ];
+
+  for (var i = 0; i < numItems; i++) {
+    var colour = 'rgb(' +
+      Math.round(rgb1[0] + (i * steps[0])) + ',' +
+      Math.round(rgb1[1] + (i * steps[1])) + ',' +
+      Math.round(rgb1[2] + (i * steps[2])) + ')';
+    list.childNodes[i].style.background = colour;
+  }
+
+  $('newList').style.background = 'rgb(98,179,25)';
+  $('newItem').style.background = 'rgb(98,179,25)';
+  $('listHeader').style.background ='rgb(98,179,25)';
+}
+
+var DB_NAME = 'done';
+
+var Done = function() {
+
+  this.db = new PouchDB(DB_NAME);
+
+  this.currentList = null;
+  // Items in a list have their own index (so they can be reordered)
+  // new items get the highest index so we keep track of the highest
+  // seen here
+  this.currentIndex = 0;
+
+  this.config = {};
+
+  this.db.get('_local/config', (function(err, config) {
+    this.config = config;
+    this.addEvents();
+    this.listenToChanges();
+    Janus.session().then(function(session) {
+      console.log(session);
+      if (session && !session.error) {
+        console.log('logged in! sync');
+        this.db.sync(session.db, {live: true});
+      }
+      return this.hashChanged();
+    }.bind(this)).then(function() {
+      // Lets not show the user the animations and
+      // item reordering on startup
+      document.body.style.display = 'block';
+    });
+  }).bind(this));
+};
+
+Done.prototype.getUser = function() {
+  return new Promise(function(resolve) {
+    var http = new XMLHttpRequest();
+    var url = 'http://127.0.0.1:3000/user';
+    http.open('GET', url, true);
+    http.setRequestHeader('Connection', 'close');
+    http.send();
+    resolve();
+  });
+};
+
+Done.prototype.addEvents = function() {
+  window.addEventListener('hashchange', this.hashChanged.bind(this));
+  $('newList').addEventListener('submit', this.createNewList.bind(this));
+  $('newItem').addEventListener('submit', this.createNewItem.bind(this));
+  $('syncSetup').addEventListener('submit', this.syncLogin.bind(this));
+  $('sync').addEventListener('click', this.toggleSyncPanel.bind(this));
+  $('items').addEventListener('click', this.toggleDone.bind(this));
+};
+
+Done.prototype.toggleSyncPanel = function() {
+  $('syncPanel').classList.toggle('hidden');
+};
+
+Done.prototype.syncLogin = function(evt) {
+  evt.preventDefault();
+
+  var email = document.getElementById('email').value.trim();
+  Janus.login({
+    email: email
+  });
+
+  var notification = 'You have been sent an email with a token to login. ' +
+    'Click on the link in the email and your data will sync with done.gd';
+  alert(notification);
+  this.toggleSyncPanel();
+};
+
+Done.prototype.createNewList = function(evt) {
+  evt.preventDefault();
+  var name = $('newListText').value;
+  if (!name || name === '') {
+    return;
+  }
+  newListText.value = '';
+  document.location.hash = '#/list/' + name + '/';
+};
+
+Done.prototype.toggleDone = function(evt) {
+  var id = evt.target.dataset.id;
+  if (!id || evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  this.db.get(id).then((function(doc) {
+    doc.status = (doc.status == 'done') ? 'todo' : 'done';
+    doc.updated = Date.now();
+    this.db.put(doc);
+  }).bind(this));
+};
+
+Done.prototype.editItem = function(id) {
+  var li = document.querySelector('[data-id="' + id + '"]');
+  var db = this.db;
+  var self = this;
+
+  db.get(id).then(function(doc) {
+    var pos = li.getBoundingClientRect();
+    items.style.transform = 'translate(0, -' + pos.top + 'px)';
+    document.body.classList.add('editing');
+    li.innerHTML = '';
+    var input = document.createElement('input');
+    input.value = doc.title;
+    li.appendChild(input);
+    input.focus();
+    input.addEventListener('blur', function() {
+      window.location.hash = '#/list/' + doc.category;
+      items.style.transform = '';
+      document.body.classList.remove('editing');
+      doc.title = input.value;
+      doc.updated = Date.now();
+      db.put(doc);
+    });
+  });
+};
+
+Done.prototype.listenToChanges = function() {
+  this.db.changes({
+    since: 'now',
+    live: true,
+    include_docs: true
+  }).on('change', (function(change) {
+    if (change.doc.category !== this.currentList) {
+      return;
+    }
+    this.processChange(change.doc);
+  }).bind(this));
+};
+
+Done.prototype.hashChanged = function() {
+
+  var url = window.location.hash.slice(1);
+  if (!url || url === '') {
+    url = '/list/default';
+  }
+
+  document.body.dataset.url = url;
+
+  if (url === '/home/') {
+    return this.showLists();
+  } else if (url.lastIndexOf('/list/', 0) === 0) {
+    var parts = url.split('/');
+    var promise = this.showList(parts[2]);
+    if (parts[3] === 'edit') {
+      this.editItem(parts[4]);
+    }
+    return promise;
+  }
+}
+
+Done.prototype.showLists = function() {
+  return new Promise(function(resolve) {
+
+    var query = {
+      reduce: '_sum',
+      map: function(doc) {
+        if (doc.category &&
+            doc.status && doc.status === 'todo') {
+          emit(doc.category, 1);
+        }
+      }
+    };
+
+    this.db.query(query, {group_level: 1}).then(function(result) {
+      lists.innerHTML = '';
+      result.rows.forEach(function(obj) {
+        var a = document.createElement('a');
+        var span = document.createElement('span');
+        var li = document.createElement('li');
+        a.setAttribute('href', '#/list/' + obj.key + '/');
+        a.textContent = obj.key;
+        span.textContent = obj.value;
+        li.appendChild(a);
+        li.appendChild(span);
+        lists.appendChild(li);
+      });
+      updateBackground(lists);
+      resolve();
+    }.bind(this));
+
+  }.bind(this));
+}
+
+Done.prototype.createNewItem = function(evt) {
+  evt.preventDefault();
+  var title = $('newItemText').value.trim();
+  if (!title || title === '') {
+    return;
+  }
+  var item = this.newTodo({
+    title: newItemText.value,
+    category: this.currentList
+  });
+  this.db.put(item);
+  newItemText.value = '';
+  newItemText.blur();
+}
+
+/*
+ * Show a particular list
+ */
+Done.prototype.showList = function(category) {
+  return new Promise((function(resolve) {
+
+    if (category === this.currentList) {
+      return resolve();
+    }
+
+    $('doneItems').innerHTML = '';
+    $('todoItems').innerHTML = '';
+
+    this.currentIndex = 0;
+    this.currentList = category;
+
+    listName.textContent = this.currentList;
+
+    var map = function(doc) {
+      if (doc.category) {
+        emit(doc.category);
+      }
+    };
+
+    var self = this;
+    this.db.query({map: map}, {
+      include_docs: true,
+      startkey: this.currentList,
+      endkey: this.currentList + '{}'
+    }).then(function(result) {
+      result.rows.forEach(function(doc) {
+        self.processChange(doc.doc);
+      });
+      resolve();
+    });
+
+  }).bind(this));
+};
+
+/*
+ * Process a change, this is used for both incoming changes and to draw
+ * the individual lists, it means we can handle real time updates and
+ * dont need to redraw entire lists for a single change.
+ */
+Done.prototype.processChange = function(doc) {
+
+  if (doc.index > this.currentIndex) {
+    this.currentIndex = doc.index;
   }
 
   var item = document.querySelector('[data-id="' + doc._id + '"]');
@@ -61,9 +310,10 @@ function processChange(doc) {
   }
 
   // We store seperate lists for done and active items
-  var toAdd = (doc.status === 'done') ? doneItems : todoItems;
+  var toAdd = (doc.status === 'done') ? $('doneItems') : $('todoItems');
 
-  // Complete items are ordered by time last updated
+  // Complete items are ordered by time last updated, active items
+  // are ordered by index
   var compare = function(dataset) {
     return (doc.status === 'done')
       ? (parseInt(dataset.updated, 10) || 0) <= doc.updated
@@ -95,207 +345,23 @@ function processChange(doc) {
   }
 
   updateBackground(todoItems);
-}
-
-function updateBackground(list) {
-  var rgb1 = [118,195,45];
-  var rgb2 = [195,237,131];
-  var numItems = list.childNodes.length;
-  var steps = [
-    (rgb2[0] - rgb1[0]) / numItems,
-    (rgb2[1] - rgb1[1]) / numItems,
-    (rgb2[2] - rgb1[2]) / numItems
-  ];
-
-  for (var i = 0; i < numItems; i++) {
-    var colour = 'rgb(' +
-      Math.round(rgb1[0] + (i * steps[0])) + ',' +
-      Math.round(rgb1[1] + (i * steps[1])) + ',' +
-      Math.round(rgb1[2] + (i * steps[2])) + ')';
-    list.childNodes[i].style.background = colour;
-  }
-
-  newListForm.style.background = 'rgb(98,179,25)';
-  newItemForm.style.background = 'rgb(98,179,25)';
-  listHeader.style.background ='rgb(98,179,25)';
-}
-
-var DB_NAME = 'done';
-
-var Done = function() {
-
-  this.db = new PouchDB(DB_NAME);
-  this.currentList = null;
-  this.config = {};
-
-  this.db.get('config', (function(err, config) {
-    this.config = config;
-    this.addEvents();
-    this.listenToChanges();
-    this.handleEvent({type: 'hashchange'});
-  }).bind(this));
 };
 
-Done.prototype.addEvents = function() {
-  window.addEventListener('hashchange', this);
-  newListForm.addEventListener('submit', this);
-  newItemForm.addEventListener('submit', this);
-  items.addEventListener('click', this);
-};
-
-Done.prototype.handleEvent = function(evt) {
-  switch (evt.type) {
-  case 'hashchange':
-    this.urlChanged(document.location.hash.slice(1));
-    break;
-  case 'submit':
-    evt.preventDefault();
-    var action = evt.target.dataset.action;
-    if (action === 'newList') {
-      return this.createNewList(newListText.value);
-    }
-    if (action === 'newItem') {
-      return this.createNewItem(newItemText.value);
-    }
-    break;
-  case 'click':
-    var action = evt.target.dataset.action;
-    if (action === 'edit') {
-      return this.editItem(evt.target, evt.target.dataset.id);
-    }
-    if (action === 'done') {
-      return this.toggleDone(evt.target.dataset.id);
-    }
-    break;
-  }
-};
-
-Done.prototype.createNewList = function(name) {
-  if (!name || name === '') {
-    return;
-  }
-  newListText.value = '';
-  document.location.hash = '#/list/' + name + '/';
-};
-
-Done.prototype.toggleDone = function(id) {
-  this.db.get(id).then((function(doc) {
-    doc.status = (doc.status == 'done') ? 'todo' : 'done';
-    doc.updated = Date.now();
-    this.db.put(doc);
-  }).bind(this));
-};
-
-Done.prototype.editItem = function(li, id) {
-  var db = this.db;
-  db.get(id).then(function(doc) {
-    var pos = li.getBoundingClientRect();
-    items.style.transform = 'translate(0, -' + pos.top + 'px)';
-    document.body.classList.add('editing');
-    li.innerHTML = '';
-    var input = document.createElement('input');
-    input.value = doc.title;
-    li.appendChild(input);
-    input.focus();
-    input.addEventListener('blur', function() {
-      items.style.transform = '';
-      document.body.classList.remove('editing');
-      doc.title = input.value;
-      doc.updated = Date.now();
-      db.put(doc);
-    });
-  });
-};
-
-Done.prototype.listenToChanges = function() {
-  this.db.changes({
-    since: 'now',
-    live: true,
-    include_docs: true
-  }).on('change', (function(change) {
-    if (change.doc.category !== this.currentList) {
-      return;
-    }
-    processChange(change.doc);
-  }).bind(this));
-};
-
-Done.prototype.urlChanged = function(url) {
-  if (!url || url === '') {
-    url = '/list/default';
-  }
-
-  if (url === '/home/') {
-    this.showLists();
-  } else if (url.startsWith('/list/')) {
-    this.showList(url);
-  }
-
-  document.body.dataset.url = url
-}
-
-Done.prototype.showLists = function() {
-  var map = function(doc) {
-    if (doc.category &&
-        doc.status &&
-        doc.status === 'todo') {
-      emit(doc.category, 1);
-    }
+Done.prototype.newTodo = function(obj) {
+  var def = {
+    category: 'default',
+    status: 'todo',
+    title: '',
+    index: ++this.currentIndex,
+    created: Date.now()
   };
 
-  this.db.query({map: map, reduce: '_sum'}, {group_level: 1}).then((function(result) {
-    lists.innerHTML = '';
-    result.rows.forEach(function(obj) {
-      var a = document.createElement('a');
-      var span = document.createElement('span');
-      var li = document.createElement('li');
-      a.setAttribute('href', '#/list/' + obj.key + '/');
-      a.textContent = obj.key;
-      span.textContent = obj.value;
-      li.appendChild(a);
-      li.appendChild(span);
-      lists.appendChild(li);
-    });
-    updateBackground(lists);
-  }).bind(this));
-}
-
-Done.prototype.createNewItem = function(title) {
-  if (!title || title === '') {
-    return;
-  }
-  var item = newTodo({
-    title: newItemText.value,
-    category: this.currentList
+  Object.keys(obj).forEach(function(key) {
+    def[key] = obj[key];
   });
-  this.db.put(item);
-  newItemText.value = '';
-  newItemText.blur();
-}
 
-Done.prototype.showList = function(url) {
-  doneItems.innerHTML = '';
-  todoItems.innerHTML = '';
-  var parts = url.split('/');
-  this.currentList = parts[2];
-
-  listName.textContent = this.currentList;
-
-  var map = function(doc) {
-    if (doc.category) {
-      emit(doc.category);
-    }
-  };
-
-  this.db.query({map: map}, {
-    include_docs: true,
-    startkey: this.currentList,
-    endkey: this.currentList + '{}'
-  }).then(function(result) {
-    result.rows.forEach(function(doc) {
-      processChange(doc.doc);
-    });
-  });
+  def._id = 'items_' + def.category + '_' + def.created;
+  return def;
 };
 
 new Done();
